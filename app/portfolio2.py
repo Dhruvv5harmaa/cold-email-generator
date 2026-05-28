26861db36b49b47077bc651565ce317668528709
Dhruv Sharma <dhruvsharmaindore@gmail.com>
2026-05-28 16:29:27 +0530

    Update portfolio2.py

--- app/portfolio2.py
+++ app/portfolio2.py
@@ -14,7 +14,7 @@ class Portfolio:
 
         self.data = pd.read_csv(file_path)
 
-        self.chroma_client = chromadb.Client()
+        self.chroma_client = chromadb.PersistentClient(path="./chroma_db")
 
         self.collection = self.chroma_client.get_or_create_collection(
             name="portfolio"
